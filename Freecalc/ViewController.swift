568ae696fc91d56a3f72d448248bd1868474bb51
Leslie Bayne <lesliebayne@Leslies-MacBook-Pro.local>
2017-06-03 13:28:27 +0200

    Initial Commit

---
+++ Freecalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Freecalc
+//
+//  Created by Leslie Bayne on 2017-06-03.
+//  Copyright © 2017 Leslie Bayne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
